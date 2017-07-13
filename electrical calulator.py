d22aaa1438e90c12a14bea6cc48d0ae97014ce63
Vivek Vishwakarma <viveksallu156@gmail.com>
2017-07-13 17:51:57 +0530

    Electrical calculator additional formulas

--- electrical calulator.py
+++ electrical calulator.py
@@ -12,6 +12,7 @@ print("1.Voltage")
 print("2.Current")
 print("3.Resistance")
 print("4.Power")
+print("5.Power factor")
 
 
 
@@ -20,50 +21,62 @@ your_choice = input("please select your choices from 1/2/3/4: ")
 
 
 def Voltage(current, resistance):
-		return current * resistance #returns voltage
+	return current * resistance #returns voltage
 def Current(voltage, resistance):
-		return voltage / resistance #returns current
+	return voltage / resistance #returns current
 def Resistance(voltage, current):
-		return voltage / current #returns resistance
+	return voltage / current #returns resistance
 def Power(voltage,current): 
-		return voltage * current  #returns power
-if your_choice == 1:
+	return voltage * current  #returns power
+def Power_factor(KW,KVA):
+	KW = int(input("Enter value in KW: "))
+	KVA = int(input("Enter value in KVA: "))
+	return KW / KVA
+
+
+try:
+	if your_choice == 1:
 		current = float(input("Enter the value of current in Ampere: "))
 		resistance = float(input("Enter the value of resistance in ohms: "))
 		print("You entered current = %s Ampere and reistance = %s ohms"%(current, resistance))
 		print("Now your answer in volts is: ")
 		print(Voltage(current, resistance))
 	
-elif your_choice == 2:
+	elif your_choice == 2:
 		voltage = float(input("Enter the value of voltage in volts: "))
 		resistance = float(input("Enter the value of resistance in ohms: "))
 		print("You entered voltage = %s volts and resistance = %s ohms"%(voltage, resistance))
 		print("Now your answer in Ampere is: ")
 		print(Current(voltage, resistance))
 		
-elif your_choice == 3:
+	elif your_choice == 3:
 		current = float(input("Enter the value of current in Ampere: "))
 		voltage = float(input("Enter the value of voltage in volts: "))
 		print("You entered voltage = %s volts and current = %s Ampere"%(voltage, current))
 		print("Now your answer in Ohms is: ")
 		print(Resistance(voltage, current))
 		
-elif your_choice == 4:
+	elif your_choice == 4:
 		voltage = float(input("Enter the value of voltage in volts: "))
 		current = float(input("Enter the value of current in Ampere: "))
 		print("You entered voltage = %s volts and current = %s Ampere"%(voltage, current))
 		print("Now your answer in Watt is: ")
 		print(Power(voltage, current))
+	elif your_choice == 5:
+		print(Power_factor(KW, KVA))
 		
-else:
-	print("invalid")
+	else:
+		print("invalid")
 		
-
+except ValueError:
+	print("Invalid input")
+		
+		
 input("click enter to exit: " )
 
 
 if __name__ == "__main__":
-	calculator()
+	electCalculator()
 
 
 
